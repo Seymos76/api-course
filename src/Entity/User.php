0a4a351c698ce867bf2bc3c854ae644b062f01db
hapinow <tech@hapinow.fr>
2020-02-02 18:37:49 +0100

    set jwt_auth with lexik bundle: auth with .pem keys + firewalls and ACLs

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -12,7 +12,11 @@ use Symfony\Component\Validator\Constraints as Assert;
 use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 
 /**
- * @ApiResource()
+ * @ApiResource(
+ *     normalizationContext={
+ *       "groups"={"users_read"}
+ *     }
+ * )
  * @UniqueEntity("email",message="Cet utilisateur existe déjà")
  * @ORM\Entity(repositoryClass="App\Repository\UserRepository")
  */
@@ -27,7 +31,7 @@ class User implements UserInterface
 
     /**
      * @ORM\Column(type="string", length=180, unique=true)
-     * @Groups({"customers_read", "invoices_read"})
+     * @Groups({"customers_read", "invoices_read", "users_read"})
      * @Assert\NotBlank(message="L'email est obligatoire")
      * @Assert\Email(message="Le format de l'email est invalide.")
      */
@@ -48,7 +52,7 @@ class User implements UserInterface
 
     /**
      * @ORM\Column(type="string", length=255)
-     * @Groups({"customers_read", "invoices_read"})
+     * @Groups({"customers_read", "invoices_read", "users_read"})
      * @Assert\NotBlank(message="Le prénom est obligatoire")
      * @Assert\Length(min="3",minMessage="Le prénom doit faire entre 3 et 255 caractères.")
      * @Assert\Length(max="255", maxMessage="Le prénom doit faire entre 3 et 255 caractères.")
@@ -57,7 +61,7 @@ class User implements UserInterface
 
     /**
      * @ORM\Column(type="string", length=255)
-     * @Groups({"customers_read", "invoices_read"})
+     * @Groups({"customers_read", "invoices_read", "users_read"})
      * @Assert\NotBlank(message="Le nom est obligatoire")
      * @Assert\Length(min="3",minMessage="Le nom doit faire entre 3 et 255 caractères.")
      * @Assert\Length(max="255", maxMessage="Le nom doit faire entre 3 et 255 caractères.")
